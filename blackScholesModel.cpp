c67fee9ad9282b4d7d5b71a2fceacd02265d9cfb
premiumfriedrice <lloyddalba@gmail.com>
2024-01-07 23:21:44 -0600

    Initilized variables for model

--- blackScholesModel.cpp
+++ blackScholesModel.cpp
@@ -1,8 +1,20 @@
 #include <string>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int d1(){
+}
+
 int main(){
+    double C;  // Call option price
+    double N;  // Cumulative Distribution function of the normal distribution
+    double St;  // Spot price or current price of an asset
+    double K;   // Strike price, price at which owner of option can exercise rights
+    double r;   // Risk-free interest rate
+    double t;   // Time to maturity or expiration
+    double o;   // Volatility of the asset
+
     cout<<"Hello World"<<endl;
 }
